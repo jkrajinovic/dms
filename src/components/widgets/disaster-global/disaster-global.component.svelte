a0f4f233c9920d2d0e9eeca5c6d2853af626cdcd
jakrajinovic <jakrajinovic@gmail.com>
2019-10-17 22:25:08 +0200

    #1 - Info about disasters added

---
+++ src/components/widgets/disaster-global/disaster-global.component.svelte
@@ -0,0 +1,32 @@
+<script>
+  let disasters = "";
+  async function getWeather() {
+    return await fetch(
+      "https://api.reliefweb.int/v1/disasters?appname=rwint-user-0&profile=list&preset=latest&slim=1&limit=6"
+    ).then(res => {
+      return res.json();
+    });
+  }
+
+  getWeather().then(result => {
+    console.log("result", result);
+    disasters = result.data;
+  });
+</script>
+
+<div class="widget global-disaster-widget block">
+  <div class="block-title">
+    <h3>Global disasters</h3>
+  </div>
+
+  <div class="card">
+    {#each disasters as disaster}
+      <div>
+        {#if disaster.fields}
+          <p>{disaster.fields.name}</p>
+        {/if}
+
+      </div>
+    {/each}
+  </div>
+</div>
